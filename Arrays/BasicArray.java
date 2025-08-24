200bc388eb49d7df366d0322f3a59e3ff053b1c1
Shreya-3110 <148695059+Shreya-3110@users.noreply.github.com>
2025-08-25 00:17:52 +0530

    Update BasicArray.java

--- Arrays/BasicArray.java
+++ Arrays/BasicArray.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+public class BasicArray {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    //Declare an array of size 5
+    int[] arr = new int[5];
+
+    //input elements
+    for(int i = 0; i<arr.length; i++){
+      arr[i] = sc.nextInt();
+    }
+
+// output elements
+    for(int i =0; i < arr.length; i++ ){
+     System.out.print(arr[i] + "");
+    }
+
+//size of array
+    System.out.println("\nSize:" + arr.length);
+    sc.close();
+      
+  }
+}
