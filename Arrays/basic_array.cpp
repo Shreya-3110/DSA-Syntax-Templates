ed5c31d2c4322af58e63582c57f0e33413cd794a
Shreya-3110 <148695059+Shreya-3110@users.noreply.github.com>
2025-08-25 00:02:21 +0530

    Update basic_array.cpp

--- Arrays/basic_array.cpp
+++ Arrays/basic_array.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+  //declare an array of size 5
+  int arr[5] = {1,2,3,4,5}
+  
+}
+
+
+for(int i = 0; i<5; i++) {
+  cin >> arr[i];
+}
+
+//Output elements
+for(int i =0; i < 5; i++) {
+  cout << arr[i] <<"";
+}
+
+
+//size of array 
+int n = sizeof(arr) / sizeof(arr[0]);
+cout<< "\nSize:" << n;
+
+return 0;
+}
+ 
