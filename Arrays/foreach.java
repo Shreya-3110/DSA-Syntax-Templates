fe768c2007fdde5566fb67dc9adf57e2e72bf743
Shreya-3110 <148695059+Shreya-3110@users.noreply.github.com>
2025-08-25 00:28:06 +0530

    Create foreach.java

---
+++ Arrays/foreach.java
@@ -0,0 +1,11 @@
+public class ArrayExample {
+  public static void main(String[] args) {
+    int[] arr = {10,20,30,40} {
+
+
+      System.out.println("Traversing using for- each loop:");
+      for(int x : arr) {
+        System.out.print(x + "");
+      }
+    }
+  }
